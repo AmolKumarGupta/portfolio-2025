9b5d0a7ad23a9046ced6372bfa2d91463696f301
amol <amlo1232001@gmail.com>
2025-12-20 23:15:09 +0530

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,20 @@ export default function RootLayout({
 						{/* </div> */}
 					</TooltipProvider>
 				</ThemeProvider>
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+					async
+				/>
+				<Script
+					id="gtag-init"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}');`,
+					}}
+				/>
 			</body>
 		</html>
 	);
